9614251108a5759a1fde3ad3d583dfc5ae4e64d4
AndreyUrazaev <f.a.z.a@bk.ru>
2025-03-27 11:39:44 +0500

    logout1 - commit1

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,17 @@ import React from "react";
 import "./App.css";
 
 function App() {
+  const logoutHandler = () => {
+    alert("Logout");
+  };
+
   return (
     <div>
       <h1>Login</h1>
       <input type="text" placeholder="enter your login" />
       <h1>Profile</h1>
       <img src="#" alt="image not found" />
+      <button onClick={logoutHandler}>Logout</button>
     </div>
   );
 }
